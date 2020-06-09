49ffe8d1b408b386e498bfaa458d73c7dae91246
QiZhang <jason.qizhang.b@gmail.com>
2020-06-10 07:52:18 +0800

    Added edit cate function

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -23,8 +23,8 @@
           <el-tag type="success" v-else-if="scope.row.cat_level === 1" size="mini">二级</el-tag>
           <el-tag type="warning" v-else-if="scope.row.cat_level === 2" size="mini">三级</el-tag>
         </template>
-        <template slot="option">
-          <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+        <template slot="option" slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditCateDialog(scope.row.cat_id)">编辑</el-button>
           <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
         </template>
       </tree-table>
@@ -40,13 +40,26 @@
           <el-input v-model="addCateForm.cat_name"></el-input>
         </el-form-item>
         <el-form-item label="父级分类：">
-          <el-cascader v-model="selectedKeys" :options="parentCateList" :props="parentCateListCascaderProps" @change="parentCateListChange" clearable>
+          <el-cascader v-model="selectedKeys" :options="parentCateList" :props="parentCateListCascaderProps"
+            @change="parentCateListChange" clearable>
           </el-cascader>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="isAddCateDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 编辑分类对话框 -->
+    <el-dialog title="编辑分类" :visible.sync="isEditCateDialogVisible" width="40%" @closed="editCateDialogClosed">
+      <el-form :model="editCateForm" :rules="editCateFormRules" ref="editCateFormRef" label-width="100px">
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="editCateForm.cat_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="isEditCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editCate">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -103,7 +116,14 @@ export default {
         children: 'children',
         checkStrictly: true
       },
-      selectedKeys: []
+      selectedKeys: [],
+      isEditCateDialogVisible: false,
+      editCateForm: {},
+      editCateFormRules: {
+        cat_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' }
+        ]
+      }
     }
   },
   created: function () {
@@ -158,6 +178,28 @@ export default {
       this.selectedKeys = []
       this.addCateForm.cat_pid = 0
       this.addCateForm.cat_level = 0
+    },
+    showEditCateDialog: async function (id) {
+      const { data: res } = await this.$http.get('categories/' + id)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.editCateForm = res.data
+      this.editCateForm.cat_id = id
+      this.isEditCateDialogVisible = true
+    },
+    editCate: function () {
+      this.$refs.editCateFormRef.validate(async isValidated => {
+        if (!isValidated) return
+        const { data: res } = await this.$http.put('categories/' + this.editCateForm.cat_id, {
+          cat_name: this.editCateForm.cat_name
+        })
+        if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        this.$message.success(res.meta.msg)
+        this.getCateList()
+        this.isEditCateDialogVisible = false
+      })
+    },
+    editCateDialogClosed: function () {
+      this.$refs.editCateFormRef.resetFields()
     }
   }
 }
